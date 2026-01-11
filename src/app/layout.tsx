6239211e2741a5d97ff85414314680aeb0877f4c
lalexeyev <leo@alexeyev.org>
2026-01-10 21:11:50 -0800

    Add vercel analytics and speed insights integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 //root layout which contains sidebar component and main content area (page.tsx)
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -10,6 +12,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <Sidebar />
           <main id="main-scroll" className="h-screen w-1/2 overflow-y-auto scroll-smooth">
             {children}
+            <Analytics />
+            <SpeedInsights />
           </main>
         </div>
       </body>
